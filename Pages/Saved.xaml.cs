c86ec0e0ce377f3bdbf51e07fd654080a2a6f58d
j3rzxy <j3rzxy@DESKTOP-BH9V1PR>
2026-04-08 21:50:36 +0300

    Создана логика страницы сохранений.

--- Pages/Saved.xaml.cs
+++ Pages/Saved.xaml.cs
@@ -16,13 +16,58 @@ using System.Windows.Shapes;
 namespace ISIP223_Bulatov_WPF.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для Saved.xaml
+    /// Логика взаимодействия для AssembliesListPage.xaml
     /// </summary>
-    public partial class Saved : Page
+    public partial class AssembliesListPage : Page
     {
-        public Saved()
+        public AssembliesListPage()
         {
             InitializeComponent();
+            RefreshData();
         }
+        private void RefreshData()
+        {
+            try
+            {
+                LbAssemblies.ItemsSource = null;
+                var list = Core.Context.assembly_.ToList();
+
+                LbAssemblies.ItemsSource = list;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при загрузке данных: " + ex.Message);
+            }
+        }
+
+        private void BtnView_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedAssembly = (sender as Button).DataContext as assembly_;
+
+            if (selectedAssembly != null)
+            {
+                Core.CurrentAssembly = selectedAssembly;
+                NavigationService.Navigate(new ConfiguratorPage());
+            }
+        }
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Вы уверены, что хотите удалить эту сборку?", "Подтверждение удаления", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                var assemblyId = (int)(sender as Button).Tag;
+                var ssembly = Core.Context.assembly_.FirstOrDefault(a => a.id == assemblyId);
+                var assemplyParts = Core.Context.partassembly_.Where(pa => pa.assemblyid == assemblyId).ToList();
+                foreach (var part in assemplyParts)
+                {
+                    Core.Context.partassembly_.Remove(part);
+                }
+                Core.Context.assembly_.Remove(ssembly);
+                Core.Context.SaveChanges();
+            }
+            RefreshData();
+        }
+
     }
 }
